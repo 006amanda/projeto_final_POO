a4e08557963cb7a9f5ced50969108a6a70352a59
006amanda <amandarooscosta@gmail.com>
2025-11-24 01:04:02 -0300

    Novo commit.

--- Trabalho_Final/src/MenuPrincipal/MenuPrincipal.java
+++ Trabalho_Final/src/MenuPrincipal/MenuPrincipal.java
@@ -1,25 +1,13 @@
 package MenuPrincipal;
+
 import java.util.*;
-import Menus.MenuUsuarios;
-import Persistencia.PersistenciaDados;
-import Persistencia.PersistenciaFactory;
-import Usuarios.*;
 import Menus.*;
+import Lancamentos.*;
 
 public class MenuPrincipal {
     public static void main(String[] args){
-
-        // INÍCIO DA PERSISTÊNCIA
-        PersistenciaDados persistTxt = PersistenciaFactory.criar("txt");
-        PersistenciaDados persistJson = PersistenciaFactory.criar("json");
-        System.out.println("Carregando dados...");
-        persistTxt.carregar();
-        persistJson.carregar();
-
-        // MENU
         Scanner sc = new Scanner(System.in);
         int opc = 0;
-
         while(opc != 7) {
             System.out.println("SISTEMA FINANCEIRO");
             System.out.println("1 - Usuários");
@@ -34,38 +22,36 @@ public class MenuPrincipal {
             System.out.println();
 
             if (opc == 1) {
-                System.out.println("Menu de Usuários");
+                System.out.println("Usuários");
                 MenuUsuarios.exibir();
             }
-            //        else if(opc == 2){
-            //            System.out.println("Menu de Contas");
-            //            MenuContas.exibir();
-            //        }
-            //        else if(opc == 3){
-            //            System.out.println("Lançamentos");
-            //            MenuLancamentos.exibir();
-            //        }
-            //        else if(opc == 4){
-            //            System.out.println("Metas");
-            //            MenuMetas.exibir();
-            //        }
-            //        else if(opc == 5){
-            //            System.out.println("Relatórios");
-            //            MenuRelatorios.exibir();
-            //        }
-            //        else if(opc == 6){
-            //            System.out.println("Simulações");
-            //            MenuSimulacoes.exibir();
-            //        }
+            else if(opc == 2){
+                System.out.println("Contas");
+                MenuContas.exibir();
+            }
+            else if(opc == 3){
+                System.out.println("Lançamentos");
+                MenuLancamentos.exibir();
+            }
+            else if(opc == 4){
+                System.out.println("Metas");
+                MenuMetas.exibir();
+            }
+            else if(opc == 5){
+                System.out.println("Relatórios");
+                GestorDeLancamentos gestor = MenuLancamentos.getGestor();
+                MenuRelatorios.setGestor(gestor);
+                MenuRelatorios.exibir();
+            }
+            else if(opc == 6){
+                System.out.println("Simulações");
+                MenuSimulacoes.exibir();
+            }
             else if (opc == 7) {
                 System.out.println("Saindo do sistema...");
             } else {
                 System.out.println("Opção inválida! ");
             }
         }
-
-        persistTxt.salvar();
-        persistJson.salvar();
-        System.out.println("Os dados foram salvos com sucesso.");
     }
 }
